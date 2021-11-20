32ac3734d5e293dfb1830485d38086bb1a7130be
neefrankie <neefrankie@gmail.com>
2021-11-20 17:36:05 +0800

    more operators and keywords

--- token/token.go
+++ token/token.go
@@ -9,13 +9,19 @@ const (
 	// EOF standards for "end of file"
 	EOF TokenType = "EOF"
 
-	// IDENT is an identifiers + literals
-	IDENT TokenType = "IDENT"
-	INT   TokenType = "INT"
-
 	// ASSIGN Operator
-	ASSIGN TokenType = "="
-	PLUS   TokenType = "+"
+	ASSIGN   TokenType = "="
+	PLUS     TokenType = "+"
+	MINUS    TokenType = "-"
+	BANG     TokenType = "!"
+	ASTERISK TokenType = "*"
+	SLASH    TokenType = "/"
+
+	LT TokenType = "<"
+	GT TokenType = ">"
+
+	EQ     TokenType = "=="
+	NOT_EQ TokenType = "!="
 
 	// COMMA Delimiters
 	COMMA     TokenType = ","
@@ -26,12 +32,39 @@ const (
 	LBRACE TokenType = "{"
 	RBRACE TokenType = "}"
 
+	// IDENT is an identifiers + literals
+	IDENT TokenType = "IDENT"
+	INT   TokenType = "INT"
+
 	// FUNCTION Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 type Token struct {
 	Type    TokenType
 	Literal string
+}
+
+var keywords = map[string]TokenType{
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
 }
