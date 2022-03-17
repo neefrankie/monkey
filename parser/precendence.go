39e676cdab15f993c3831d6bf7ddeac204a82e2f
weiguo.ni <neefrankie@gmail.com>
2022-03-17 16:46:40 +0800

    feat: precedence

---
+++ parser/precendence.go
@@ -0,0 +1,41 @@
+package parser
+
+import "monkey/token"
+
+const (
+	_ int = iota
+	LOWEST
+	EQUALS      // ==
+	LESSGREATER // > <
+	SUM         // + -
+	PRODUCT     // * /
+	PREFIX      // ! -
+	CALL
+)
+
+var precedences = map[token.TokenType]int{
+	token.EQ:       EQUALS,
+	token.NOT_EQ:   EQUALS,
+	token.LT:       LESSGREATER,
+	token.GT:       LESSGREATER,
+	token.PLUS:     SUM,
+	token.MINUS:    SUM,
+	token.SLASH:    PRODUCT,
+	token.ASTERISK: PRODUCT,
+}
+
+func (p *Parser) peekPrecedence() int {
+	if p, ok := precedences[p.peekToken.Type]; ok {
+		return p
+	}
+
+	return LOWEST
+}
+
+func (p *Parser) curPrecedence() int {
+	if p, ok := precedences[p.curToken.Type]; ok {
+		return p
+	}
+
+	return LOWEST
+}
