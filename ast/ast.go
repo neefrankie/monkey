b593778e515d2574902967625b0a008869087820
weiguo.ni <neefrankie@gmail.com>
2022-03-17 16:44:23 +0800

    feat: Program

--- ast/ast.go
+++ ast/ast.go
@@ -1,5 +1,9 @@
 package ast
 
+import (
+	"bytes"
+)
+
 type Node interface {
 	TokenLiteral() string
 	String() string
@@ -13,4 +17,29 @@ type Statement interface {
 type Expression interface {
 	Node
 	expressionNode()
+}
+
+// Program is the root node of every AST our parser produces.
+// Every valid Monkey program is a series of statements.
+// These statements are contained in the Statements.
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
 }
