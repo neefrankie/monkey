0781a6afb97dc87cac65197264fcbe77a067d462
neefrankie <neefrankie@gmail.com>
2022-04-10 18:58:30 +0800

    evaluate prefix

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -6,9 +6,7 @@ import (
 )
 
 var (
-	NULL  = &object.Null{}
-	TRUE  = &object.Boolean{Value: true}
-	FALSE = &object.Boolean{Value: true}
+	NULL = &object.Null{}
 )
 
 func Eval(node ast.Node) object.Object {
@@ -26,6 +24,10 @@ func Eval(node ast.Node) object.Object {
 
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
 	}
 
 	return nil
@@ -39,12 +41,4 @@ func evalStatements(stmts []ast.Statement) object.Object {
 	}
 
 	return result
-}
-
-func nativeBoolToBooleanObject(input bool) *object.Boolean {
-	if input {
-		return TRUE
-	}
-
-	return FALSE
 }
