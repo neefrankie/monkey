d31dd7a26bba97b6dc544cff8b53bdbf44e9478e
neefrankie <neefrankie@gmail.com>
2022-04-16 18:02:12 +0800

    evaluate return

--- object/object.go
+++ object/object.go
@@ -3,9 +3,10 @@ package object
 type Type string
 
 const (
-	INTEGER_OBJ Type = "INTEGER"
-	BOOLEAN_OBJ Type = "BOOLEAN"
-	NULL_OBJ    Type = "NULL"
+	INTEGER_OBJ      Type = "INTEGER"
+	BOOLEAN_OBJ      Type = "BOOLEAN"
+	NULL_OBJ         Type = "NULL"
+	RETURN_VALUE_OBJ Type = "RETURN_VALUE"
 )
 
 type Object interface {
---
+++ object/return.go
@@ -0,0 +1,13 @@
+package object
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv ReturnValue) Type() Type {
+	return RETURN_VALUE_OBJ
+}
+
+func (rv ReturnValue) Inspect() string {
+	return rv.Value.Inspect()
+}
