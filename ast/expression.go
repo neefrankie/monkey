cc091080fc2ad94df27c579791c1fd941e17ce7c
neefrankie <neefrankie@gmail.com>
2022-04-16 17:34:27 +0800

    fix: missing space after if string

--- ast/expression.go
+++ ast/expression.go
@@ -126,7 +126,7 @@ func (ie *IfExpression) TokenLiteral() string {
 func (ie *IfExpression) String() string {
 	var out bytes.Buffer
 
-	out.WriteString("if")
+	out.WriteString("if ")
 	out.WriteString(ie.Condition.String())
 	out.WriteString(" ")
 	out.WriteString(ie.Consequence.String())
