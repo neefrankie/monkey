72682c57a18e902f1a1feaa423511cca0308bbe0
neefrankie <neefrankie@gmail.com>
2022-04-24 16:46:57 +0800

    function literal

--- ast/expression.go
+++ ast/expression.go
@@ -139,35 +139,6 @@ func (ie *IfExpression) String() string {
 	return out.String()
 }
 
-type FunctionLiteral struct {
-	Token      token.Token
-	Parameters []*Identifier
-	Body       *BlockStatement
-}
-
-func (fl *FunctionLiteral) expressionNode() {}
-
-func (fl *FunctionLiteral) TokenLiteral() string {
-	return fl.Token.Literal
-}
-
-func (fl *FunctionLiteral) String() string {
-	var out bytes.Buffer
-
-	var params []string
-	for _, p := range fl.Parameters {
-		params = append(params, p.String())
-	}
-
-	out.WriteString(fl.TokenLiteral())
-	out.WriteString("(")
-	out.WriteString(strings.Join(params, ", "))
-	out.WriteString(") ")
-	out.WriteString(fl.Body.String())
-
-	return out.String()
-}
-
 type CallExpression struct {
 	Token     token.Token
 	Function  Expression
