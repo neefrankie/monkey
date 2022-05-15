394c60f7c657c4c768df737a5b6742fa79d4d928
neefrankie <neefrankie@gmail.com>
2022-05-15 20:14:01 +0800

    evaluate index expression

--- evaluator/eval_expressions.go
+++ evaluator/eval_expressions.go
@@ -21,3 +21,24 @@ func evalExpressions(
 
 	return result
 }
+
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObject := array.(*object.Array)
+	idx := index.(*object.Integer).Value
+	max := int64(len(arrayObject.Elements) - 1)
+
+	if idx < 0 || idx > max {
+		return NULL
+	}
+
+	return arrayObject.Elements[idx]
+}
