79ac589c1dd3ca01e5de3a12d89971d20018f3e8
neefrankie <neefrankie@gmail.com>
2022-05-15 16:23:55 +0800

    add ArrayLiteral

--- ast/expression.go
+++ ast/expression.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"monkey/token"
+	"strings"
 )
 
 // Identifier holds the identifier in a binding like x
@@ -50,6 +51,32 @@ func (sl *StringLiteral) TokenLiteral() string {
 
 func (sl *StringLiteral) String() string {
 	return sl.Token.Literal
+}
+
+type ArrayLiteral struct {
+	Token    token.Token
+	Elements []Expression
+}
+
+func (al *ArrayLiteral) expressionNode() {}
+
+func (al *ArrayLiteral) TokenLiteral() string {
+	return al.Token.Literal
+}
+
+func (al *ArrayLiteral) String() string {
+	var out bytes.Buffer
+
+	elements := []string{}
+	for _, el := range al.Elements {
+		elements = append(elements, el.String())
+	}
+
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+
+	return out.String()
 }
 
 // PrefixExpression holds prefix operators.
